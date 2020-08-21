e78f9833b1700ababe671422f039debe132c041a
jasonnonstop10 <jay44411@gmail.com>
2020-08-21 22:03:54 +0700

    [Add] upload image, save Image and display image

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def predictImage(request):
+    print(request)
+    print(request.POST.dict())
+    fileObj=request.FILES['filePath']
+    fs=FileSystemStorage()
+    filePathName=fs.save(fileObj.name,fileObj)
+    filePathName=fs.url(filePathName)
+    context={'filePathName':filePathName}
+    return render(request,'index.html',context)
