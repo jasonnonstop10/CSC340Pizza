418765a55f9f30a51b74fe0f455fdd77e56e8370
jasonnonstop10 <jay44411@gmail.com>
2020-08-30 13:31:35 +0700

    [add]+[bug] deploy model in app

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import json
+from tensorflow import Graph
+
+img_height, img_width=224,224
+with open('./model/pizza_model.json','r') as f:
+    labelInfo=f.read()
+
+labelInfo=json.loads(labelInfo)
+
+model_graph = Graph()
+with model_graph.as_default():
+    tf_session = tf.compat.v1.Session()
+    with tf_session.as_default():
+        model=load_model('./model/weights.best.hdf5')
+
+
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -12,5 +32,17 @@ def predictImage(request):
     fs=FileSystemStorage()
     filePathName=fs.save(fileObj.name,fileObj)
     filePathName=fs.url(filePathName)
-    context={'filePathName':filePathName}
+    testimage='.'+filePathName
+    img = image.load_img(testimage, target_size=(img_height, img_width))
+    x = image.img_to_array(img)
+    x=x/255
+    x=x.reshape(1,img_height, img_width,3)
+    with model_graph.as_default():
+        with tf_session.as_default():
+            predi=model.predict(x)
+
+    import numpy as np
+    predictedLabel=labelInfo[str(np.argmax(predi[0]))]
+
+    context={'filePathName':filePathName,'predictedLabel':predictedLabel[1]}
     return render(request,'index.html',context)
